96e388c71d76f4ad49a13f343e03a79ae97d2dbf
Alexis Diaz <alexdan1993@gmail.com>
2021-12-09 11:35:10 -0300

    Refactor app index

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import reducer from './reducers'
-import createSagaMiddleware from 'redux-saga'
-import rootSaga from './sagas'
 import Tasks from './components/Tasks'
 import styled from 'styled-components'
-
-const sagaMiddleware = createSagaMiddleware()
-export const store = createStore(reducer, applyMiddleware(sagaMiddleware))
-sagaMiddleware.run(rootSaga)
+import Filters from './components/Filters'
+import { selectTasks } from './reducers/tasks'
+import { useSelector } from 'react-redux'
 
 function App() {
+  const tasks = useSelector(selectTasks)
   return (
-    <Provider store={store}>
-      <Container>
-        <h1>todos</h1>
-        <Tasks />
-      </Container>
-    </Provider>
+    <Container>
+      <h1>todos</h1>
+      <Tasks />
+      <Footer>
+        <Count>{tasks.length} items left</Count>
+        <Filters />
+      </Footer>
+    </Container>
   )
 }
 
@@ -38,6 +35,21 @@ const Container = styled.div`
     font-weight: 300;
     margin-bottom: 30px;
   }
+`
+
+const Footer = styled.footer`
+  background-color: white;
+  border-top: 1px solid #e6e6e6;
+  color: #777;
+  height: 40px;
+  padding: 10px;
+  position: relative;
+  width: 100%;
+`
+
+const Count = styled.span`
+  left: 20px;
+  position: absolute;
 `
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import createSagaMiddleware from 'redux-saga'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import reducer from './reducers'
+import rootSaga from './sagas'
+import './index.css'
+
+const sagaMiddleware = createSagaMiddleware()
+const composedEnhancers = composeWithDevTools(applyMiddleware(sagaMiddleware))
+export const store = createStore(reducer, composedEnhancers)
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
